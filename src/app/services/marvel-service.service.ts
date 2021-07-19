122bd28b20c76428bdbce6e3bd87efcd56bbf495
JohnMoran <johnnymoran44@hotmail.com>
2021-07-19 19:10:36 +0200

    using proxy to overcome the cors

--- src/app/services/marvel-service.service.ts
+++ src/app/services/marvel-service.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-const API_URL = 'https://reqres.in';
+
+const KEY = 'b4ae5f9b470feb926a9ac769fa9de7fa';
+const Private = '19979debb41903c961cf2ed6340adecb7b710f8e';
 
 @Injectable({
   providedIn: 'root',
@@ -11,6 +13,10 @@ export class MarvelServiceService {
   constructor(private http: HttpClient) {}
 
   public get(url: string): Observable<any> {
-    return this.http.get(API_URL + '/api/' + url).pipe(map((res) => res));
+    return this.http.get(`/comics?apikey=${KEY}`).pipe(map((res) => res));
   }
 }
+
+//data.results[13].images[0].path
+//http://i.annihil.us/u/prod/marvel/i/mg/c/60/4bc69f11baf75.jpg
+//http://i.annihil.us/u/prod/marvel/i/mg/f/c0/4bc66d78f1bee.jpg
